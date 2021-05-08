9b2825ad536b04ad9d23942a108935ba63fea7fe
sandypockets <zac@znacreative.com>
2021-05-07 21:20:32 -0400

    Add basic story creation functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,10 +2,17 @@ class StoriesController < ApplicationController
 
   # Stories list page (also homepage)
   def index
+    @stories = Story.all
   end
 
   def new
+    @story = Story.new
+  end
 
+  def create
+    @story = Story.new(params.require(:story).permit(:title, :body, :topic))
+    @story.save
+    redirect_to root_path
   end
 
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ApplicationRecord
+
+
+
 end
