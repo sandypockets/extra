502e18aa9ef91efe56ce6b5fcb2bd38aa0c66d52
sandypockets <zac@znacreative.com>
2021-05-08 17:23:52 -0400

    Use story title as URL

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,9 @@ class Story < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
+  # Changing story URL from number to story title
+  def to_param
+    id.to_s + '-' + title.parameterize
+  end
+
 end
