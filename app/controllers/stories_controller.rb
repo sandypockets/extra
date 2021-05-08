6b068bdd35476d60c8770f585004260614357e68
sandypockets <zac@znacreative.com>
2021-05-07 20:49:36 -0400

    Add index and new views to stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,11 @@
 class StoriesController < ApplicationController
+
+  # Stories list page (also homepage)
+  def index
+  end
+
+  def new
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Routes for stories
   resources :stories
+
+  # Set homepage as stories index page
+  root 'stories#index'
 
 end
