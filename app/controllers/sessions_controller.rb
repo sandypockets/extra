f3a92caf683361e98de770c7cc247e0fba37c537
sandypockets <zac@znacreative.com>
2021-05-09 14:22:34 -0400

    Add logout with reset_session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+  # Login form
+  def new; end
+
+  # Login action
+  def create
+    @form_data = params.require(:session)
+    @email = @form_data[:email]
+    @password = @form_data[:password]
+    @user = User.find_by(email: @email).try(:authenticate, @password)
+    if @user.present?
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render 'sessions/new'
+    end
+  end
+
+  # Logout
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+
 end
