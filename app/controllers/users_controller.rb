9709d45e5e2d119d934d3be40859eee66b546ac1
sandypockets <zac@znacreative.com>
2021-05-09 12:35:42 -0400

    Create remaining controller routes. Sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,38 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(form_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render 'users/new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(form_params)
+      redirect_to user_path(@user)
+    else
+      render 'users/edit'
+    end
+  end
+
+  def form_params
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
   end
 
 end
