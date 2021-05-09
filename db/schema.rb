a4bf7e5edb667b0db765d3587233800ee4b8178b
sandypockets <zac@znacreative.com>
2021-05-09 11:46:20 -0400

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_09_043230) do
+ActiveRecord::Schema.define(version: 2021_05_09_153315) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2021_05_09_043230) do
     t.string "title"
     t.text "body"
     t.string "topic"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
