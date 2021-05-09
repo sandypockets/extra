cc1d855dd7576d8006a2d7250573c8f6b0807b1a
sandypockets <zac@znacreative.com>
2021-05-09 00:56:18 -0400

    Set up controller route to create comment on story

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+  def create
+    @story = Story.find(params[:story_id])
+    @comment = @story.comments.new(params.require(:comment).permit(:body))
+    @comment.save
+    redirect_to story_path(@story)
+  end
+
 end
