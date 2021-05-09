58099c829089d8eb228ca1966b7db3fc3de674bb
sandypockets <zac@znacreative.com>
2021-05-09 15:59:44 -0400

    Add methods to check if user is logged in, and store them as @current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  before_action :find_current_user
+
+  # Make method available to other views
+  helper_method :has_session?
+
+  def logged_in
+    redirect_to new_session_path unless has_session?
+  end
+
+  def find_current_user
+    @current_user = (User.find(session[:user_id]) if has_session?)
+  end
+
+  def has_session?
+    session[:user_id].present?
+  end
+
 end
