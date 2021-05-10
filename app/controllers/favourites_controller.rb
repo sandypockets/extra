2c6682b79d821cc38ed4e969eeeba88fbc05a860
sandypockets <zac@znacreative.com>
2021-05-09 23:20:21 -0400

    Set up favouriting/bookmarking feature

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,21 @@
 class FavouritesController < ApplicationController
+
+  before_action :logged_in, except: :show
+
+  def show; end
+
+  def create
+    @story = Story.find(params[:story_id])
+    @favourite = @story.favourites.new
+    @favourite.user = @current_user
+    @favourite.save
+    redirect_to story_path(@story)
+  end
+
+  def destroy
+    @story = Story.find(params[:story_id])
+    @story.favourites.where(user: @current_user).delete_all
+    redirect_to story_path(@story)
+  end
+
 end
