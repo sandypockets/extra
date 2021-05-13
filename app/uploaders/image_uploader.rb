db9df841fcc8974aeddf69562e2374dfe2e1952b
sandypockets <zac@znacreative.com>
2021-05-13 14:39:41 -0400

    Adjust image resize values

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,20 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+
+  # Size to fit the max width and height
+  # process resize_to_fit: [1000, 800]
+  process resize_to_fit: [800, 800]
+
+
+  version :thumb do
+    process resize_to_fill: [150, 150]
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
