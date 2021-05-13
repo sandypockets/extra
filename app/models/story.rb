6d5d3763529a3a222d5d28f049944ff796030ac7
sandypockets <zac@znacreative.com>
2021-05-13 12:52:11 -0400

    Mount image uploader

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,11 +1,15 @@
 class Story < ApplicationRecord
 
+
   # A story can have many comments
   has_many :comments
   # A story can have many favourites
   has_many :favourites
   # A story can only belong to one user
   belongs_to :user
+
+  # Add image uploader
+  mount_uploader :image, ImageUploader
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
