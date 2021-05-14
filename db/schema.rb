aeaa1c1f28c3195bd43e0472072eff2e9b0a5001
sandypockets <zac@znacreative.com>
2021-05-14 11:04:16 -0400

    Add admin boolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_163651) do
+ActiveRecord::Schema.define(version: 2021_05_14_145551) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_163651) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_admin", default: false
   end
 
   add_foreign_key "comments", "stories"
