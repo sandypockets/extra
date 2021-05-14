54358af8787828a4eb5331dd5e9f46c3672d7377
sandypockets <zac@znacreative.com>
2021-05-14 12:59:19 -0400

    Permit params

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Page do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :body, :url, :image
+  permit_params :title, :body, :url, :image
   #
   # or
   #
