647482bd849fed0301a673173dab1f7e5278c6b2
sandypockets <zac@znacreative.com>
2021-05-14 13:20:47 -0400

    Add controller for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,18 @@
 class PagesController < ApplicationController
+
+  # Be sure to add a route after creating the controller def
+
+
+  def about
+    @content = Page.find_by(url: 'about')
+  end
+
+  def our_story
+    @content = Page.find_by(url: 'our-story')
+  end
+
+  def terms
+    @content = Page.find_by(url: 'terms-and-conditions')
+  end
+
 end
