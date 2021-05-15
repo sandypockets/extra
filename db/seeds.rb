45fe4cd9aed7c829660e73a2ed4a0020358bf62b
sandypockets <zac@znacreative.com>
2021-05-15 12:44:00 -0400

    Comment seed example

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 9.times do |i|
+#   Story.create(
+#     title: "Lorem Ipsum #{i + 1}",
+#     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+#     topic: 'Ideas',
+#     user_id: 3
+#   )
+# end
